27cb7e71d44050b4d561044d713862acd28dbc87
NikitaMelnychencko <hunter6059@gmail.com>
2021-11-06 02:34:11 +0200

    add_base_config
    add_base_config

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,23 @@
 import '../sass/index.scss';
-import exampl from '../templates/exampl.hbs'
 
-
+//==========call_list===========
+import './call_list/call_list';
+//==========components===========
+import './components/backdrop';
+import './components/block_help';
+import './components/fetch';
+import './components/logo';
+import './components/modal_login';
+import './components/pagination-list';
+import './components/scroll_too';
+import './components/color_switcher';
+//=========layout===============
+//=========static===============
+import './layout/static/header';
+import './layout/static/footer';
+//=========main================
+import './layout/gallery';
+import './layout/modal_one_movie';
+import './layout/modal_team_list';
+import './layout/hero_home';
+import './layout/hero_my_list';
