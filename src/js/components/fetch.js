033551191441a7502778fc434f0d7303b53b5ead
bondarchugtr <81237327+bondarchugtr@users.noreply.github.com>
2021-11-06 15:16:38 +0200

    d

---
+++ src/js/components/fetch.js
@@ -0,0 +1,27 @@
+
+export function apiServiceHomePage(page) {
+    return fetch(`https://api.themoviedb.org/3/trending/movie/week?api_key=843d6905879c9b52f41f5f6a1e2c8966&page=${page}`)
+        .then(response => {
+            console.log(response);
+
+            return response.json();
+        }).catch(error => alert(errorNot));
+};
+
+
+export function apiServiceSeorch(searchQuery, page) {
+    return fetch(`https://api.themoviedb.org/3/search/movie?api_key=843d6905879c9b52f41f5f6a1e2c8966&language=en-US&page=${page}&include_adult=false`)
+        .then(response => {
+            console.log(response);
+            return response.json();
+        }).catch(error => alert(errorNot));
+};
+
+export function apiServiceSeorchParam() {
+    return fetch(`https://api.themoviedb.org/3/movie/{movie_id}?api_key=843d6905879c9b52f41f5f6a1e2c8966&language=en-US`)
+
+        .then(response => {
+            console.log(response);
+            return response.json();
+        }).catch(error => alert(errorNot));
+};
