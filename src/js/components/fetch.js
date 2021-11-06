c3b54987f0de7ae74873a659e8d836b0f3160893
bondarchugtr <81237327+bondarchugtr@users.noreply.github.com>
2021-11-06 21:51:08 +0200

    refact fetch

--- src/js/components/fetch.js
+++ src/js/components/fetch.js
@@ -1,25 +1,21 @@
 
-export function apiServiceHomePage(page) {
-    return fetch(`https://api.themoviedb.org/3/trending/movie/week?api_key=843d6905879c9b52f41f5f6a1e2c8966&page=${page}`)
-        .then(response => {
-            console.log(response);
-            return response.json();
-        }).catch(error => alert(errorNot));
-};
+export function renderMuvieGlobal(page, searchQuery, movie_id, options) {
+    if (options === 'home') { // Для рендера Вызов функции renderMuvieGlobal(page, '', '', home) для страници "Home"
+        const BASEURL = `https://api.themoviedb.org/3/trending/movie/week?api_key=843d6905879c9b52f41f5f6a1e2c8966&page=${page}`
+        return baseFetch(BASEURL);
 
 
-export function apiServiceSeorch(searchQuery, page) {
-    return fetch(`https://api.themoviedb.org/3/search/movie?api_key=843d6905879c9b52f41f5f6a1e2c8966&query=${searchQuery}&page=${page}`)
+    } else if (searchQuery !== undefined) { //Для рендера Вызов функции renderMuvieGlobal(page, searchQuery) для "input"
+        const BASEURL = (`https://api.themoviedb.org/3/search/movie?api_key=843d6905879c9b52f41f5f6a1e2c8966&query=${searchQuery}&page=${page}`)
+        return baseFetch(BASEURL);
+    } else {
+        const BASEURL = (`https://api.themoviedb.org/3/movie/{movie_id}?api_key=843d6905879c9b52f41f5f6a1e2c8966&language=en-US`)
+        return baseFetch(BASEURL);
+    }
+}
+function baseFetch(BASEURL) {
+    return fetch(BASEURL)
         .then(response => {
-            console.log(response);
             return response.json();
         }).catch(error => alert(errorNot));
-};
-
-export function apiServiceSeorchParam() {
-    return fetch(`https://api.themoviedb.org/3/movie/{movie_id}?api_key=843d6905879c9b52f41f5f6a1e2c8966&language=en-US`)
-        .then(response => {
-            console.log(response);
-            return response.json();
-        }).catch(error => alert(errorNot));
-};
+}
