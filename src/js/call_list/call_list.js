aeb09d16be31fa1b26cf28c707e289e7424dc3e7
NikitaMelnychencko <hunter6059@gmail.com>
2021-11-07 17:51:22 +0200

    +

--- src/js/call_list/call_list.js
+++ src/js/call_list/call_list.js
@@ -2,3 +2,8 @@ import main from '../../views/layout/main.hbs'
 import { refs } from '../refs/refs.js'
 //test
 refs.main.innerHTML = main();
+
+// function homeRender() {
+//   refs.main.innerHTML = main({homeMarkUp});
+// }
+// homeRender()
