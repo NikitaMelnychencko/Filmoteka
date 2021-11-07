418ca31428e6aeeecc6514afac395f48439e24af
tikkasandy <tikkasandy@gmail.com>
2021-11-07 15:04:52 +0200

    Add homepage markup

--- src/js/call_list/call_list.js
+++ src/js/call_list/call_list.js
@@ -1,11 +1,17 @@
 import main from '../../views/layout/main.hbs'
 import card from '../../views/components/card_galery.hbs'
 import { refs } from '../refs/refs.js'
+import { renderMovieGlobal } from '../components/fetch.js'
 //test
 refs.main.innerHTML = main();
-//refs.card.innerHTML = card();
 
-const gallery = document.querySelector('.gallery-list')
-console.log(card)
-gallery.insertAdjacentHTML('afterbegin', card());
-//console.log(card());
+const gallery = document.querySelector('.gallery-list');
+
+function renderCards(data) {
+    const movies = data.results;
+    gallery.insertAdjacentHTML('afterbegin', card(movies));
+}
+
+const page = 1;
+
+renderMovieGlobal(page, '', '', 'home').then(renderCards);
--- src/js/refs/refs.js
+++ src/js/refs/refs.js
@@ -1,5 +1,4 @@
 export const refs = {
   main: document.querySelector('main'),
   footerLink: document.querySelector('[data-action="open-modal-teamlist"]'),
-  //card: document.querySelector('.gallery-list')
 };
