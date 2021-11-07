524e8b84f0d48e585e2813ef81a876b96d7689a3
tikkasandy <tikkasandy@gmail.com>
2021-11-07 11:00:51 +0200

    Make markup gallery card

--- src/js/call_list/call_list.js
+++ src/js/call_list/call_list.js
@@ -1,4 +1,11 @@
 import main from '../../views/layout/main.hbs'
+import card from '../../views/components/card_galery.hbs'
 import { refs } from '../refs/refs.js'
 //test
 refs.main.innerHTML = main();
+//refs.card.innerHTML = card();
+
+const gallery = document.querySelector('.gallery-list')
+console.log(card)
+gallery.insertAdjacentHTML('afterbegin', card());
+//console.log(card());
--- src/js/refs/refs.js
+++ src/js/refs/refs.js
@@ -1,4 +1,5 @@
 export const refs = {
   main: document.querySelector('main'),
   footerLink: document.querySelector('[data-action="open-modal-teamlist"]'),
+  //card: document.querySelector('.gallery-list')
 };
