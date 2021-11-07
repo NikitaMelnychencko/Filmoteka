e4f2407918b6af16a68295b567adb1b155854f8d
NikitaMelnychencko <hunter6059@gmail.com>
2021-11-07 17:53:45 +0200

    add-function

--- src/js/call_list/call_list.js
+++ src/js/call_list/call_list.js
@@ -1,4 +1,10 @@
 import main from '../../views/layout/main.hbs'
 import { refs } from '../refs/refs.js'
 //test
-refs.main.innerHTML = main();
+function homeRender() {
+  refs.main.innerHTML = main({homeMarkUp});
+}
+homeRender()
+function myLibraryRender() {
+  refs.main.innerHTML = main({homeMarkUp});
+}
