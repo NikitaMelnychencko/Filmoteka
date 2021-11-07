037231f3a284b51c72b65f92beeb1b17198724f3
NikitaMelnychencko <hunter6059@gmail.com>
2021-11-07 23:26:03 +0200

    +

--- src/js/call_list/call_list.js
+++ src/js/call_list/call_list.js
@@ -1,14 +1,16 @@
+// example
+// pageRender(mainTittle.home)
+// pageRender(mainTittle.my_library_watched)
+// pageRender(mainTittle.my_library_queue)
+
 import main from '../../views/layout/main.hbs'
 import { refs } from '../refs/refs.js'
 import mainTittle from '../data/main.json'
+
 //test
-function homeRender() {
-  const currentValue = mainTittle.home
+function pageRender(value) {
+  const currentValue = value 
   refs.main.innerHTML = main({currentValue});
 }
-homeRender()
-function myLibraryRender() {
-  const currentValue = mainTittle.my_library
-  refs.main.innerHTML = main({currentValue});
-}
-//myLibraryRender()
+
+pageRender(mainTittle.home)
