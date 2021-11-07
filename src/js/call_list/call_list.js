5f6fac8ea22839eb9d95bb3d93cd722403af57c4
NikitaMelnychencko <hunter6059@gmail.com>
2021-11-07 18:32:29 +0200

    move_img

--- src/js/call_list/call_list.js
+++ src/js/call_list/call_list.js
@@ -1,9 +1,14 @@
 import main from '../../views/layout/main.hbs'
 import { refs } from '../refs/refs.js'
+import mainTittle from '../data/main.json'
 //test
-refs.main.innerHTML = main();
-
-// function homeRender() {
-//   refs.main.innerHTML = main({homeMarkUp});
-// }
-// homeRender()
+function homeRender() {
+  const currentValue = mainTittle.home
+  refs.main.innerHTML = main({currentValue});
+}
+homeRender()
+function myLibraryRender() {
+  const currentValue = mainTittle.my_library
+  refs.main.innerHTML = main({currentValue});
+}
+//myLibraryRender()
