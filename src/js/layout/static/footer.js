3dce40222d3982ae86dcfa4122dbfa37c7237c84
VladVinichenko <vinnyxxxl925@gmail.com>
2021-11-08 01:37:50 +0200

    c

--- src/js/layout/static/footer.js
+++ src/js/layout/static/footer.js
@@ -5,3 +5,20 @@ function openTeamListModal(e) {
   e.preventDefault();
   console.log('вызов функции, шоб модалка появилась');
 }
+
+const heart = document.querySelector('.footer__svg');
+let timerFooterId = null;
+function heartBeat() {
+  timerFooterId = setInterval(intervalBeat, 1000);
+}
+
+function intervalBeat() {
+  heart.classList.add('footer__svg-heartbeat');
+  timerFooterId = setTimeout(delayBeat, 250);
+}
+
+function delayBeat() {
+  heart.classList.remove('footer__svg-heartbeat');
+  removeTimeout(delayBeat, 250);
+}
+heartBeat();
