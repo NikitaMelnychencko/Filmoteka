5493fee5d240aeece63da945d6a6be52c8384c15
bondarchugtr <81237327+bondarchugtr@users.noreply.github.com>
2021-11-08 20:47:27 +0200

    Update fetch.js

--- src/js/components/fetch.js
+++ src/js/components/fetch.js
@@ -4,6 +4,7 @@ const BASE_URL = 'https://api.themoviedb.org/3';
 const TREND_URL = `${BASE_URL}/trending/movie/week`;
 const SEARCH_URL = `${BASE_URL}/search/movie`
 const ID_URL = `${BASE_URL}/movie/`;
+const GENRE_URL = `${BASE_URL}/genre/movie`
 
 // Вызов функции renderMuvieGlobal(page, searchQuery) для "input"
 // Вызов функции renderMuvieGlobal(page, '', '', home) для страници "Home"
@@ -15,10 +16,12 @@ export function renderMuvieGlobal(page, searchQuery, id, options) {
   if (options === 'home') {
     const REQUEST_ADRESS = `${TREND_URL}?api_key=${API_KEY}&page=${page}`
     return baseFetch(REQUEST_ADRESS);
+
   } else if (searchQuery !== undefined) {
     const REQUEST_ADRESS = `${SEARCH_URL}?api_key=${API_KEY}&query=${searchQuery}&page=${page}`
     return baseFetch(REQUEST_ADRESS);
   }
+
 }
 
 export function renderParamsCard(id) {
@@ -27,6 +30,7 @@ export function renderParamsCard(id) {
   return baseFetch(REQUEST_ADRESS);
 }
 
+
 function baseFetch(REQUEST_ADRESS) {
   return fetch(REQUEST_ADRESS)
     .then(response => {
@@ -34,4 +38,10 @@ function baseFetch(REQUEST_ADRESS) {
         return response.json();
       }
     }).catch(error => alert(errorNot));
+}
+
+export function genreMovie(genre) {
+  const REQUEST_ADRESS = `${GENRE_URL}/list?api_key=${API_KEY}&language=en-US`
+  console.log(REQUEST_ADRESS);
+  return baseFetch(REQUEST_ADRESS);
 }
