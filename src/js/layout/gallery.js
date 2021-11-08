da9dd5327f877eb50bc515e55fca59c68da47927
tikkasandy <tikkasandy@gmail.com>
2021-11-08 16:48:25 +0200

    Rename variables

--- src/js/layout/gallery.js
+++ src/js/layout/gallery.js
@@ -1,16 +1,18 @@
-import genr from '../data/genres.json'
+import { genres } from '../data/genres.json'
 import img from '../../images/img/png/gallery/no-image.png'
 import card from '../../views/components/card_galery.hbs'
 import { renderMovieGlobal } from '../components/fetch.js'
 
-const genreDB = new Map(
-    genr.genres.map(i => [i.id, i.name]));
+const EMPTY_DATA = '-';
+const IMG_URL = 'https://image.tmdb.org/t/p/w500';
 
-console.log(img);
+const currentPage = 999;
 
 const gallery = document.querySelector('.gallery-list');
 
-const currentPage = 999;
+const genreDB = new Map(
+    genres.map(i => [i.id, i.name])
+);
 
 renderMovieGlobal(currentPage, '', '', 'home').then(renderCards);
 
@@ -23,10 +25,10 @@ function modifyData(data) {
     const newData = data.results;
     newData.forEach(movie => {
         movie.vote_average = movie.vote_average.toFixed(1);
-        movie.genre_ids = movie.genre_ids.length === 0 ? ["-"] : movie.genre_ids.map(genre => genreDB.get(genre));
+        movie.genre_ids = !movie.genre_ids.length ? [EMPTY_DATA] : movie.genre_ids.map(genre => genreDB.get(genre));
         movie.genre_ids = movie.genre_ids.join(", ");
-        movie.release_date = !movie.release_date ? '-' : movie.release_date.slice(0, 4);
-        movie.poster_path = !movie.poster_path ? img : `https://image.tmdb.org/t/p/w500${movie.poster_path}`
+        movie.release_date = !movie.release_date ? EMPTY_DATA : movie.release_date.slice(0, 4);
+        movie.poster_path = !movie.poster_path ? img : `${IMG_URL}${movie.poster_path}`
     });
     return newData;
 };
