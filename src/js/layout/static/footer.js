c5000ebcae3b0520dc812519717544b7c00c94b9
VladVinichenko <vinnyxxxl925@gmail.com>
2021-11-09 12:01:57 +0200

    fix heartbeat

--- src/js/layout/static/footer.js
+++ src/js/layout/static/footer.js
@@ -7,21 +7,3 @@ function openTeamListModal(e) {
   console.log('вызов функции, шоб модалка появилась');
   //!
 }
-
-const heart = document.querySelector('.footer__svg');
-let timerFooterId = null;
-function heartBeat() {
-  timerFooterId = setInterval(intervalBeat, 1000);
-}
-
-function intervalBeat() {
-  heart.classList.add('footer__svg-heartbeat');
-  timerFooterId = setTimeout(delayBeat, 150);
-}
-
-function delayBeat() {
-  heart.classList.remove('footer__svg-heartbeat');
-  clearTimeout(delayBeat, 150);
-}
-
-heartBeat();
