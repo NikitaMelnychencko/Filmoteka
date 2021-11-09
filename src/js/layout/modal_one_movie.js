dbde8a2313ec5b7e4b1716252c31eb1dc1597f28
VladVinichenko <vinnyxxxl925@gmail.com>
2021-11-09 17:13:09 +0200

    complete v1 folm-modal

--- src/js/layout/modal_one_movie.js
+++ src/js/layout/modal_one_movie.js
@@ -1,6 +1,7 @@
 import modal_one_movie_markup from '../../views/partials/modal_one_movie.hbs';
-import testcard from '../testcard.json'; //test object with movie detail
+import testcard from '../testcard.json'; //test object with movie detail. delete after start function
 import { renderModal } from '../components/modal';
-const modalContent = modal_one_movie_markup(testcard); //need insert object with movie detail
+const url = 'блаблабла';
+const modalContent = modal_one_movie_markup(testcard, url); //need insert object with movie detail
 
 renderModal(modalContent); //function open modal with string html.
