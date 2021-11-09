986d1968f7a567e45391bd0734496fc338d78a33
Yevheniia Mankivska <evgeniyamankovskaya@gmail.com>
2021-11-09 21:43:41 +0200

    input function + styles for warning

--- src/js/layout/hero_home.js
+++ src/js/layout/hero_home.js
@@ -1,3 +1,23 @@
 import heroHome from '../../views/partials/hero_home.hbs';
 import searchSvg from '../../images/svg/sprite.svg';
+import { renderMovieGlobal } from '../components/fetch.js';
 export const homeMarkUp = heroHome({ searchSvg });
+
+export function openInput() {
+  const refs = {
+    searchForm: document.querySelector('.search-form'),
+    galleryList: document.querySelector('.gallery-list'),
+    warning: document.querySelector('.warning'),
+  };
+  refs.searchForm.addEventListener('submit', onSearch);
+  function onSearch(e) {
+    e.preventDefault();
+    const searchQuery = e.currentTarget.elements.query.value;
+    const page = 1;
+    renderMovieGlobal(page, searchQuery);
+    if (searchQuery.length <= 1) {
+      return (refs.warning.textContent =
+        'Search result not successful. Enter the correct movie name and try again!');
+    }
+  }
+}
