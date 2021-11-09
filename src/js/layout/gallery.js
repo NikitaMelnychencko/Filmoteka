61ddeb673f35c9153064a24d3aeb72f323a98aca
tikkasandy <tikkasandy@gmail.com>
2021-11-09 13:46:20 +0200

    Refactoring renderGallery

--- src/js/layout/gallery.js
+++ src/js/layout/gallery.js
@@ -8,16 +8,34 @@ const IMG_URL = 'https://image.tmdb.org/t/p/w500';
 const gallery = document.querySelector('.gallery-list');
 let currentPage = undefined;
 
-init().then(renderGallery(1, '', '', 'home'));
+init();
+renderGallery();
 
-function renderGallery(page, searchQuery, id, options) {
+//renderGallery("titanic", 3);
+//renderGallery("borat");
+renderGallery('', 2);
+
+// renderGallery () - отрисовывает первую страницу самых популярных фильмов
+
+async function renderGallery(options = 'home', page = 1) {
     currentPage = page;
-    renderMovieGlobal(currentPage, searchQuery, id, options).then(renderMovies);
-    return currentPage;
+    let movies = undefined;
+    console.log(options);
+    if (!options) {
+        console.log("Ooops!");
+        return;
+    }
+    if (options === 'home') {
+        movies = (await renderMovieGlobal(currentPage, '', '', options)).results;
+    } else {
+        movies = (await renderMovieGlobal(currentPage, options, '', '')).results;
+    };
+    renderMovies(movies);
+    return movies;
 };
 
 function renderMovies(movies) {
-    const moviesData = getData(movies.results, GENRES_MAP);
+    const moviesData = getData(movies, GENRES_MAP);
     gallery.innerHTML = card(moviesData);
 };
 
@@ -27,7 +45,7 @@ function getData(movies, genres) {
             id: m.id,
             title: m.title,
             vote_average: m.vote_average.toFixed(1),
-            genres: m.genre_ids.map(id => { return { id, name: genres.get(id), url: '' } }), // TODO: Url to show movies by genre
+            genres: m.genre_ids.map(id => { return { id, name: genres.get(id), url: '' } }),
             release_date: m.release_date,
             release_date_year: m.release_date.slice(0, 4),
             poster_path: !m.poster_path ? img : `${IMG_URL}${m.poster_path}`
