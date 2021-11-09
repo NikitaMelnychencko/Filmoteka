edb6b01bcd8297485e6025862c62fc98abd65011
VladVinichenko <vinnyxxxl925@gmail.com>
2021-11-09 14:36:21 +0200

    с1

--- src/js/layout/modal_one_movie.js
+++ src/js/layout/modal_one_movie.js
@@ -3,4 +3,4 @@ import testcard from '../testcard.json'; //test object with movie detail
 import { renderModal } from '../components/modal';
 const modalContent = modal_one_movie_markup(testcard); //need insert object with movie detail
 
-// renderModal(modalContent); //function open modal with string html
+renderModal(modalContent); //function open modal with string html.
