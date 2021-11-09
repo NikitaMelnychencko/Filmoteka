f8d92c01254afc133ff0282ef52112fa03063282
VladVinichenko <vinnyxxxl925@gmail.com>
2021-11-09 17:15:20 +0200

    c

--- src/js/layout/modal_one_movie.js
+++ src/js/layout/modal_one_movie.js
@@ -4,4 +4,4 @@ import { renderModal } from '../components/modal';
 const url = 'блаблабла';
 const modalContent = modal_one_movie_markup(testcard, url); //need insert object with movie detail
 
-renderModal(modalContent); //function open modal with string html.
+// renderModal(modalContent); //function open modal with string html. test
