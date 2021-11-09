ea9aa2f217934c15004ba9adba494bd997094fbe
andreydnb <a.gritsay2@gmail.com>
2021-11-09 21:22:25 +0200

    add function open close modal

--- src/js/layout/modal_team_list.js
+++ src/js/layout/modal_team_list.js
@@ -1,51 +1,20 @@
 import teamTemplate from '/views/partials/modal_team_list.hbs';
 import team from '../data/team-list.json';
-//import { modalMarkup } from '../components/modal_login'
-const modalContent = teamTemplate();
-
-
 
 import * as basicLightbox from 'basiclightbox';
 import 'basicLightbox/src/styles/main.scss';
 
+
+import { renderModal } from '../components/modal';
+const modalTheme = teamTemplate(team); //need insert object with movie detail
+
+ //test
 // Refs
+const modalContent = teamTemplate();
 
-const modalContainer = document.querySelector('[data-action="open-modal-teamlist"]');
-
+const modalContainer =document.querySelector('[data-action="open-modal-teamlist"]')
 modalContainer.addEventListener('click', openModal);
 
-function openModal(e) {
-  e.preventDefault();
-
-  try {
-    getTeamInfo(team);
-  } catch (error) {
-    console.error('Something wrong with team modal window' + error);
-  }
-
-  
-}
-// Function for getting data from Json
-function getTeamInfo(teamId) {
-  const teamMarkup = teamTemplate(teamId);
-  const modalContent = basicLightbox.create(teamMarkup);
-
-  modalContent.show();
-
-  window.addEventListener('keydown', closeModalByEsc);
-
-  function closeModalByEsc(e) {
-    if (e.code === 'Escape') {
-      modalContent.close();
-
-      window.removeEventListener('keydown', closeModalByEsc);
-    }
-  }
-  const btnCloseRef = document.querySelector('.close__button');
-  btnCloseRef.addEventListener('click', closeModalbyBtn);
-  function closeModalbyBtn() {
-    modalContent.close();
-
-    btnCloseRef.removeEventListener('click', closeModalbyBtn);
-  }
+function openModal() {
+ renderModal(modalTheme);  
 }
