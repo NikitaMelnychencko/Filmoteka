2948629e6ac2cbe513e9ed48041b25e9f883f63c
Nikita  Melnychencko <82110006+NikitaMelnychencko@users.noreply.github.com>
2021-11-10 23:17:43 +0200

    add_test_firebase

--- src/js/test/firebase.js
+++ src/js/test/firebase.js
@@ -1,63 +1,49 @@
-// import { initializeApp } from 'firebase/app';
-// import { getAuth } from 'firebase/auth';
-// import { getFirestore, collection, getDocs } from 'firebase/firestore';
-
-// const firebaseApp = initializeApp({
-//   apiKey: "AIzaSyCrhBW63SM95ZUKCf6EsxC1CtzGhzdJBtQ",
-//   authDomain: "goit-js10-films-library.firebaseapp.com",
-//   databaseURL: "https://goit-js10-films-library-default-rtdb.europe-west1.firebasedatabase.app",
-//   projectId: "goit-js10-films-library",
-//   storageBucket: "goit-js10-films-library.appspot.com",
-//   messagingSenderId: "365608496961",
-//   appId: "1:365608496961:web:88f6a83f1a1fc4849dfbc0",
-//   measurementId: "G-5Z0M6YF9RQ"
-// });
-// const auth = getAuth(firebaseApp)
-// const db = getFirestore(firebaseApp)
-
-//   async function getUsers(db) {
-//     const citiesCol = collection(db, "goit-js10-films-library-default-rtdb")
-//     const citySnapshot = await getDocs(citiesCol);
-//     console.log(citiesCol);
-//     const cityList = citySnapshot.docs.map(doc => doc.data());
-    
-//     return cityList;
-//   }
-// getUsers(db)
-
-
 //Import the functions you need from the SDKs you need
 
-import { initializeApp } from "firebase/app";
+import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
-import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
-import { } from "firebase/database";
-import { } from "firebase/storage";
-import {  } from "firebase/messaging";
+import { getFirestore, collection, getDocs, addDoc } from 'firebase/firestore';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
- const firebaseConfig = {
-  apiKey: "AIzaSyBU3Wc3Z3R37hTZkFlOc3KVu8xNH1dedWg",
-  authDomain: "fir-test-a5c95.firebaseapp.com",
-  projectId: "fir-test-a5c95",
-  storageBucket: "fir-test-a5c95.appspot.com",
-  messagingSenderId: "182392685843",
-  appId: "1:182392685843:web:affe727b10036cb775c6e6",
-  measurementId: "G-BZY0VQZMS4"
- }
+const firebaseConfig = {
+  apiKey: "AIzaSyCrhBW63SM95ZUKCf6EsxC1CtzGhzdJBtQ",
+  authDomain: "goit-js10-films-library.firebaseapp.com",
+  databaseURL: "https://goit-js10-films-library-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "goit-js10-films-library",
+  storageBucket: "goit-js10-films-library.appspot.com",
+  messagingSenderId: "365608496961",
+  appId: "1:365608496961:web:88f6a83f1a1fc4849dfbc0",
+  measurementId: "G-5Z0M6YF9RQ"
+};
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
-// Get a list of cities from your database
-async function getCities(db) {
-  const citiesCol = collection(db, 'cities');
+
+// get
+async function getCities(db,velue) {
+  const citiesCol = collection(db, velue);
   const citySnapshot = await getDocs(citiesCol);
-  console.log(citySnapshot);
+
   const cityList = citySnapshot.docs.map(doc => doc.data());
+  console.log(cityList);
   return cityList;
 }
+getCities(db, 'users');
 
-getCities(db)
+//Post
+async function postUsers(db,value) {
+  try {
+    const docRef = await addDoc(collection(db, value), {
+      first: 'Ada',
+      last: 'Lovelace',
+      born: 1815,
+    });
+    console.log('Document written with ID: ', docRef.id);
+  } catch (e) {
+    console.error('Error adding document: ', e);
+  }
+}
+postUsers(db, 'users');
