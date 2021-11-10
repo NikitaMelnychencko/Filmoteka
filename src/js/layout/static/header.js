c870fb0fb42392c22b072b91fe21b02ee19bd671
aliev.saftar94@gmail.com <56716472+Saftar94@users.noreply.github.com>
2021-11-10 14:55:19 +0200

    Push header button
    Push header button

--- src/js/layout/static/header.js
+++ src/js/layout/static/header.js
@@ -1,21 +1,70 @@
-refs = {
-  inputSerch: document.querySelector('.header-form__text'),
-  serchButton: document.querySelector('.serch'),
+import { before, has, remove } from 'lodash';
+// import buttonHeader from '../../../views/partials/header_my_lib.hbs';
+// import modalWindow from '../../../views/partials/modal_wind.hbs';
+
+const refs = {
+  headBtn: document.querySelector('.header__button'),
+  clicMyLib: document.querySelectorAll('.nav__list'),
+  removeSerch: document.querySelector('.header-form'),
+  bodymodal: document.querySelector('.js-backdrop'),
+  closeModalBnt: document.querySelector('[data-action="close-modal"]'),
 };
 
-refs.inputSerch.addEventListener('input', inputText);
-refs.serchButton.addEventListener('click', submitFun);
-
-function inputText() {
-  if (refs.inputSerch.value === '') {
-    console.log('Yes');
-  }
+var a = document.querySelectorAll('.nav__title li a');
+for (var i = 0, length = a.length; i < length; i++) {
+  a[i].onclick = function () {
+    var b = document.querySelector('.nav__title li.nav__current');
+    if (b) b.classList.remove('nav__current');
+    this.parentNode.classList.add('nav__current');
+  };
 }
 
-function submitFun() {
-  if (refs.inputSerch.value === '') {
-    alert('Error');
-  } else {
-    inputText();
-  }
-}
+refs.clicMyLib.forEach(list => {
+  list.addEventListener('click', function (eve) {
+    eve.preventDefault();
+    if (eve.target.textContent == 'my library') {
+      refs.removeSerch.classList.add('hidden');
+      refs.headBtn.classList.remove('hidden');
+    } else if (eve.target.textContent == 'home') {
+      refs.headBtn.classList.add('hidden');
+      refs.removeSerch.classList.remove('hidden');
+    } else if (eve.target.textContent !== 'login') {
+      // Передать функцию модального окна авторизации
+
+      console.log('Open modal Auteri');
+    }
+  });
+});
+
+// function appendfotoTpl(articles) {
+//   refs.headBtn.insertAdjacentHTML('beforeend', buttonHeader(articles));
+// }
+
+// function removefotoTpl(event) {
+//   refs.headBtn.remove(buttonHeader(event));
+// }
+// eve.preventDefault()
+
+// Модальное окно
+// function onOpenModal(event) {
+//   window.addEventListener('keydown', closeEsc);
+//   refs.bodymodal.insertAdjacentHTML('beforeend', modalWindow(event));
+//   console.log('Modal');
+// }
+// function onCloseModal() {
+//   window.removeEventListener('keydown', closeEsc);
+//   document.body.classList.remove('show-modal');
+// }
+
+// function mouseCloseMOdal(event) {
+//   if (event.currentTarget === event.target) {
+//     onCloseModal();
+//   }
+// }
+
+// function closeEsc(event) {
+//   console.log(event.currentTarget);
+//   if (event.code === 'Escape') {
+//     onCloseModal();
+//   }
+// }
