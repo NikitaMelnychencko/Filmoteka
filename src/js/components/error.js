0b4e463a27cc8fa0aa8908b9f68fd3970b83853f
bondarchugtr <81237327+bondarchugtr@users.noreply.github.com>
2021-11-10 16:30:25 +0200

    Update error.js

--- src/js/components/error.js
+++ src/js/components/error.js
@@ -2,6 +2,7 @@
 import errorRend from '..//..//views/components/error.hbs';
 import img from '../../images/img/png/error/error.jpeg';
 export const imgMarkup = errorRend({ img });
+
 const errors = document.querySelector('.gallery');
 
 
