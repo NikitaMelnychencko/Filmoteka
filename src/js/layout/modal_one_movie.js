b61c141ab0ac37efb2d1c6e3b519c6f2a93a3804
andreydnb <a.gritsay2@gmail.com>
2021-11-11 23:38:57 +0200

    clickOnMovieHandler

--- src/js/layout/modal_one_movie.js
+++ src/js/layout/modal_one_movie.js
@@ -1,6 +1,35 @@
 import modal_one_movie_markup from '../../views/partials/modal_one_movie.hbs';
 import testcard from '../testcard.json'; //test object with movie detail. delete after start function
 import { renderModal } from '../components/modal';
-const modalContent = modal_one_movie_markup(testcard); //need insert object with movie detail
+import { renderParamsCard } from '../components/fetch';
+import { refs } from '../refs/refs'
+; //need insert object with movie detail
 
-renderModal(modalContent); //function open modal with string html. test
+refs.main.addEventListener('click', clickOnMovieHandler);
+
+// Click Handler Function
+async function clickOnMovieHandler(e) {
+  e.preventDefault();
+
+  if (e.target.nodeName !== 'IMG' && e.target.nodeName !== 'H3') {
+    return;
+  }
+
+    let movieId = e.target.dataset.id;
+  await fetchById(movieId);
+}
+
+async function fetchById(id) {
+
+    try {
+        const movieId = await renderParamsCard(id);
+        let modalContent = modal_one_movie_markup(testcard)
+        renderModal(modalContent);
+        console.log(movieId)
+
+        
+    } catch (error) {
+        console.error('Smth wrong with outer fetch by ID' + error);
+    }
+}
+
