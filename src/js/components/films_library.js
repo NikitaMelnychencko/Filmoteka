ff826b0792156a59654f3650cc016771ea75dc43
DeadNord <eosipopo@gmail.com>
2021-11-11 13:05:40 +0200

    =

--- src/js/components/films_library.js
+++ src/js/components/films_library.js
@@ -1,12 +1,11 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
-import { getFirestore, collection } from 'firebase/firestore';
-import { getAuth, onAuthStateChanged } from 'firebase/auth';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from 'firebase/auth';
+import { getFirestore, collection, getDocs, addDoc } from 'firebase/firestore';
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: 'AIzaSyCrhBW63SM95ZUKCf6EsxC1CtzGhzdJBtQ',
   authDomain: 'goit-js10-films-library.firebaseapp.com',
@@ -18,91 +17,62 @@ const firebaseConfig = {
   appId: '1:365608496961:web:88f6a83f1a1fc4849dfbc0',
   measurementId: 'G-5Z0M6YF9RQ',
 };
+
 const app = initializeApp(firebaseConfig);
-
 const db = getFirestore(app);
-const auth = getAuth(app);
 
-async function getCities(db) {
-  const citiesCol = collection(db, 'users');
-  const citySnapshot = await getDocs(citiesCol);
-  const cityList = citySnapshot.docs.map(doc => doc.data());
-  return cityList;
+const email = 'test@gmail.com';
+const password = 'tesdadt1';
+const auth = getAuth();
+
+// Aut User
+// get
+async function getUsers(db, value) {
+  const usersCol = collection(db, value);
+  const usersSnapshot = await getDocs(usersCol);
+  const usersList = usersSnapshot.docs.map(doc => doc.data());
+  console.log(usersSnapshot);
+  return usersList;
 }
-// const usersList = collection(db, 'users');
 
-// console.log(cityList);
+signInWithEmailAndPassword(auth, email, password)
+  .then(userCredential => {
+    const user = userCredential.user;
+    const userId = user.reloadUserInfo.localId;
+    // console.log(userId.reloadUserInfo.localId);
+    // getUsers(db, `users`);
+  })
+  .catch(error => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+  });
 
-//function to save file
-// function previewFile() {
-//   const storage = firebase.storage();
+// const arrayUser = signInWithEmailAndPassword((auth, email, password));
 
-//   const file = document.getElementById('files').files[0];
-//   console.log(file);
-
-//   const storageRef = firebase.storage().ref();
-
-//   //dynamically set reference to the file name
-//   const thisRef = storageRef.child(file.name);
-
-//   //put request upload file to firebase storage
-//   thisRef.put(file).then(function (snapshot) {
-//     console.log('Uploaded a blob or file!');
+// Reg User
+// createUserWithEmailAndPassword(auth, email, password)
+//   .then(userCredential => {
+//     // Signed in
+//     const user = userCredential.user;
+//     // ...
+//   })
+//   .catch(error => {
+//     const errorCode = error.code;
+//     const errorMessage = error.message;
+//     // ..
 //   });
 
-//   //get request to get URL for uploaded file
-//   thisRef.getDownloadURL().then(function (url) {
-//     console.log(url);
-//   });
-// }
-
-// // // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-// const db = getFirestore(app);
-
-// async function getUsers(db) {
-//   const usersCol = collection(db, 'users');
-//   const usersSnapshot = await getDocs(usersCol);
-//   const usersList = usersSnapshot.docs.map(doc => doc.data());
-//   return usersList;
-// }
-
-// getUsers('users');
-
-// Регестраци, получение айди и создание базы юзера
-// Авторизация, получение айди и поолучение доступа к базе юзера
-
-// // const email = 'myemail@email.com';
-// // const password = 'mypassword';
-
-// // firebase
-// //   .auth()
-// //   .signInWithEmailAndPassword(email, password)
-// //   .catch(function (error) {
-// //     console.log(error.code);
-// //     console.log(error.message);
-// //   });
-
-// // const ref = firebase.database().ref('users');
-
-// // console.log(ref);
-// export class Question {
-//   static create(question) {
-//     return fetch(input: 'https://goit-js10-films-library-default-rtdb.europe-west1.firebasedatabase.app/',
-//       init: {
-//         method: 'POST',
-//         body: JSON.stringify(question),
-//         headers: {
-//           'Content-Type': 'application/json'
-//         }
-//     })
-//       .then(onfulfilled: response => response.json())
-//       .then(onfulfilled: response => {
-//       console.log(response)
-//     })
-//   }
-// }
-
-// function getUsersFromLocalStorage() {
-//   return JSON.parse(text: localStorage.getItem(key: 'users') || ]'[]')
-// }
+//Post
+async function postUsers(db, value) {
+  try {
+    const docRef = await addDoc(collection(db, value), {
+      first: 'Ada',
+      last: 'Lovelace',
+      born: 1815,
+    });
+    console.log('Document written with ID: ', docRef.id);
+  } catch (e) {
+    console.error('Error adding document: ', e);
+  }
+}
+//postUsers(db, 'users');
--- src/js/test/firebase.js
+++ src/js/test/firebase.js
@@ -9,21 +9,22 @@ import { getFirestore, collection, getDocs, addDoc } from 'firebase/firestore';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCrhBW63SM95ZUKCf6EsxC1CtzGhzdJBtQ",
-  authDomain: "goit-js10-films-library.firebaseapp.com",
-  databaseURL: "https://goit-js10-films-library-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "goit-js10-films-library",
-  storageBucket: "goit-js10-films-library.appspot.com",
-  messagingSenderId: "365608496961",
-  appId: "1:365608496961:web:88f6a83f1a1fc4849dfbc0",
-  measurementId: "G-5Z0M6YF9RQ"
+  apiKey: 'AIzaSyCrhBW63SM95ZUKCf6EsxC1CtzGhzdJBtQ',
+  authDomain: 'goit-js10-films-library.firebaseapp.com',
+  databaseURL:
+    'https://goit-js10-films-library-default-rtdb.europe-west1.firebasedatabase.app',
+  projectId: 'goit-js10-films-library',
+  storageBucket: 'goit-js10-films-library.appspot.com',
+  messagingSenderId: '365608496961',
+  appId: '1:365608496961:web:88f6a83f1a1fc4849dfbc0',
+  measurementId: 'G-5Z0M6YF9RQ',
 };
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
 // get
-async function getUsers(db,velue) {
+async function getUsers(db, velue) {
   const usersCol = collection(db, velue);
   const usersSnapshot = await getDocs(citiesCol);
 
@@ -34,7 +35,7 @@ async function getUsers(db,velue) {
 //getUsers(db, 'users');
 
 //Post
-async function postUsers(db,value) {
+async function postUsers(db, value) {
   try {
     const docRef = await addDoc(collection(db, value), {
       first: 'Ada',
