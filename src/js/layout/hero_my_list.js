0a7d20efaa7cd8e17eddbf6f385b57299195e70a
Oksana Soroka <oksanabodnaruk1507@gmail.com>
2021-11-14 20:06:21 +0200

    add changes

--- src/js/layout/hero_my_list.js
+++ src/js/layout/hero_my_list.js
@@ -1,1 +1,45 @@
+import { pageRender} from '../call_list/call_list';
+import mainTittle from '../data/main.json';
+import { getUser, userId } from '../components/films_library';
 
+let userListArrey
+
+function getrefs() {
+    const refs = {
+galleryTitle: document.querySelector('.gallery__tittle--hide')
+    }
+    return refs;
+}
+
+
+
+export function onLibButtons() {
+    const buttonsList = document.querySelector('.hero-library__buttons-thumb');
+    buttonsList.addEventListener('click', onButtonsLibClick)
+}
+
+function onButtonsLibClick(event) {
+    if (event.target.nodeName !== 'BUTTON') { return }
+    btnIsActive(event.target);
+    userListArrey = getUser(`${userId}`, `${event.target.dataset.action}`);
+   
+    if (event.target.classList.contains('hero-library__button--watched')) {
+    renameGalleryTitle(mainTittle.my_library_watched.gallery_title)
+    }
+    else {
+    renameGalleryTitle(mainTittle.my_library_queue.gallery_title)
+     }
+ }
+
+ 
+function btnIsActive(element) {
+    const activeElement = document.querySelector('.is-active')
+    if (activeElement) {
+       activeElement.classList.remove('is-active') 
+    }
+    element.classList.add('is-active'); 
+}
+
+function renameGalleryTitle(value) {
+    getrefs().galleryTitle.innerHTML = value;    
+}
