25f65cfd7dc427a5169a763fc8508b75e7b784bf
VladVinichenko <vinnyxxxl925@gmail.com>
2021-11-15 12:57:10 +0200

    e

--- src/js/components/backdrop.js
+++ src/js/components/backdrop.js
@@ -12,7 +12,7 @@ export function renderBackdrop() {
 
 export function closeBackdrop() {
   backdrop().classList.remove('backdrop_is-open');
-  document.documentElement.style.overflow = 'overlay';
+  document.documentElement.style.overflow = 'auto';
   backdrop().removeEventListener('click', closeMd);
 }
 
