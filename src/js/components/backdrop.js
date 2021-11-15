017229f68a8c2f431c0d384c9522619c9d6c653a
VladVinichenko <vinnyxxxl925@gmail.com>
2021-11-15 13:18:39 +0200

    MDN scroll fix

--- src/js/components/backdrop.js
+++ src/js/components/backdrop.js
@@ -4,6 +4,16 @@ function backdrop() {
   return backdrop;
 }
 
+//from MDN
+function getBrowserId() {
+  let aKeys = ['MSIE', 'Firefox', 'Safari', 'Chrome', 'Opera'],
+    sUsrAg = navigator.userAgent,
+    nIdx = aKeys.length - 1;
+  for (nIdx; nIdx > -1 && sUsrAg.indexOf(aKeys[nIdx]) === -1; nIdx--);
+  return nIdx;
+}
+//from MDN
+
 export function renderBackdrop() {
   backdrop().classList.add('backdrop_is-open');
   document.documentElement.style.overflow = 'hidden';
@@ -12,8 +22,17 @@ export function renderBackdrop() {
 
 export function closeBackdrop() {
   backdrop().classList.remove('backdrop_is-open');
-  document.documentElement.style.overflow = 'auto';
+  restoreScroll();
   backdrop().removeEventListener('click', closeMd);
+}
+
+function restoreScroll() {
+  if (getBrowserId() === 1) {
+    document.documentElement.style.overflow = 'auto';
+    return;
+  }
+  document.documentElement.style.overflow = 'overlay';
+  return;
 }
 
 function closeMd(evt) {
