91af5e2ed3138c5f35e0045f9eb98684350094c4
aliev.saftar94@gmail.com <56716472+Saftar94@users.noreply.github.com>
2021-11-17 01:36:43 +0200

    push bagfix
    push bagfix

--- src/js/components/modal_login.js
+++ src/js/components/modal_login.js
@@ -6,40 +6,38 @@ import {
   user,
 } from './appFirebase';
 import { refs } from '../refs/refs.js';
-import { addSpinner,removeSpinner } from './spinner';
-
+import { addSpinner, removeSpinner } from './spinner';
 
 refs.formLog.addEventListener('submit', e => {
   e.preventDefault();
-  addSpinner()
-  const formData = new FormData(e.currentTarget)
+  addSpinner();
+  const formData = new FormData(e.currentTarget);
   const emailValue = formData.get('email');
   const passValue = formData.get('pass');
-  signInUser(emailValue, passValue)
-  clearInput(refs.formLog, 2)
-  removeSpinner()
-  addClass()
-})
+  signInUser(emailValue, passValue);
+  clearInput(refs.formLog, 2);
+  removeSpinner();
+  addClass();
+});
 
 refs.formReg.addEventListener('submit', e => {
   e.preventDefault();
-  addSpinner()
-  const formData = new FormData(e.currentTarget)
+  addSpinner();
+  const formData = new FormData(e.currentTarget);
   const emailValue = formData.get('email');
   const passValue = formData.get('pass');
   const nameValue = formData.get('name');
-  regUser(emailValue, passValue)
-  updateInUser(nameValue)
-  AuthState(user)
-  clearInput(refs.formReg, 3)
-  removeSpinner()
-  addClass()
-})
+  regUser(emailValue, passValue);
+  updateInUser(nameValue);
+  AuthState(user);
+  clearInput(refs.formReg, 3);
+  removeSpinner();
+  addClass();
+});
 
-
-function clearInput(ref,number) {
-  for (let i = 0; i < number; i++) { 
-    ref.children[i].children[1].value=''
+function clearInput(ref, number) {
+  for (let i = 0; i < number; i++) {
+    ref.children[i].children[1].value = '';
   }
 }
 
@@ -48,42 +46,41 @@ function clearInput(ref,number) {
 refs.singUP.addEventListener('click', openSinUp);
 
 function openSinUp(eve) {
-  // eve.preventDefault();
   const item = eve.target.textContent.trim();
-  console.log(item);
   if (item === 'Sign up Now') {
-    refs.singOutMod.classList.remove('hidden');
-    refs.singInMod.classList.add('hidden');
+    refs.singOutMod.classList.remove('modal-singup--hidden');
+    refs.singInMod.classList.add('modal-singin--hidden');
   }
 }
 
 // function close modal
 
 window.addEventListener('keydown', onCloseModal);
-window.addEventListener('click', mouseCloseMOdal);
+refs.singinModal.addEventListener('click', mouseCloseMOdal);
 
 function onCloseModal(eve) {
-  const cli = eve.code;
-  if (cli === 'Escape') {
-    refs.sininModal.classList.add('hidden');
+  if (eve.code === 'Escape') {
+    refs.singinModal.classList.add('modal-auth--hidden');
   }
 }
 
 function mouseCloseMOdal(event) {
-  if (event.target.className === 'backdrop-sing'||event.target.className === 'cl-btn-mod-txt') {
-    addClass()
+  if (
+    event.target.className === 'backdrop-sing' ||
+    event.target.className === 'cl-btn-mod-txt'
+  ) {
+    addClass();
   }
 }
-function addClass() {
-  refs.sininModal.classList.add('hidden');
-}
-// back sing Up
 
+function addClass() {
+  refs.singinModal.classList.add('modal-auth--hidden');
+}
+
+// back sing Up
 refs.backModal.addEventListener('click', backSingOut);
 
 function backSingOut(event) {
-  const item = event;
-  refs.singOutMod.classList.add('hidden');
-  refs.singInMod.classList.remove('hidden');
+  refs.singOutMod.classList.add('modal-singup--hidden');
+  refs.singInMod.classList.remove('modal-singin--hidden');
 }
-
