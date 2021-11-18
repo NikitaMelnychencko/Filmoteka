7b32571e6146855e11e7b9d1a8b82920038071e5
bondarchugtr <81237327+bondarchugtr@users.noreply.github.com>
2021-11-19 01:32:43 +0200

    +

--- src/js/filter/filter_sort.js
+++ src/js/filter/filter_sort.js
@@ -1,3 +1,4 @@
+import { child } from 'firebase/database';
 import { renderGallery } from '../layout/gallery';
 
 let sort = '';
@@ -145,10 +146,13 @@ function removeOpen() {
 
 // animation Arrow input
 function onToggleArrowInput(evt) {
-    console.log(evt.target);
-    if (evt.target) {
-        refsFilter().arrowInput.classList.toggle('transform');
-    }
+    // console.log(refsFilter().listSort.classList('.open'));
+
+    console.log(evt.nextElementSibling);
+    // if (evt.target === child.classList === 'open') {
+
+    //     refsFilter().arrowInput.classList.toggle('transform');
+    // }
 }
 
 
