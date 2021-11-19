cca5381acd61c5c7ad7eab9c122312bf2ed1b855
NikitaMelnychencko <hunter6059@gmail.com>
2021-11-19 16:46:54 +0200

    +

--- src/js/components/preloader_bar.js
+++ src/js/components/preloader_bar.js
@@ -3,4 +3,7 @@ import { refs } from '../refs/refs.js';
 export function preloaderIsHided() {
   refs.preloader.classList.add('loading--hiden')
 }
+export function preloaderDisable() {
+  refs.preloader.style.display = 'none'
+} 
  
