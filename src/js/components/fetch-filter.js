d162e296cd6237fcf964b12192ac92cf5e73aa9d
NikitaMelnychencko <hunter6059@gmail.com>
2021-11-19 04:25:05 +0200

    +

--- src/js/components/fetch-filter.js
+++ src/js/components/fetch-filter.js
@@ -4,25 +4,25 @@ const BASE_URL = 'https://api.themoviedb.org/3';
 
 export function filterGlobal(popularityDesc, popularityAsc, releaseDateDesc, releaseDateAsk, releaseTitleDesc, releaseTitleAsk) {
     if (popularityDesc === 'popularity.desc') {
-        const REQUEST_ADRESS = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=${popularityDesc}&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate`
+        const REQUEST_ADRESS = `${BASE_URL}/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=${popularityDesc}&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate`
         return baseFetch(REQUEST_ADRESS);
     } else if (popularityAsc === 'popularity.asc') {
-        const REQUEST_ADRESS = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=${popularityAsc}&&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate`
+        const REQUEST_ADRESS = `${BASE_URL}/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=${popularityAsc}&&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate`
         return baseFetch(REQUEST_ADRESS);
 
     } else if (releaseDateDesc === 'primary_release_date.desc') {
-        const REQUEST_ADRESS = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=${releaseDateDesc}&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate`
+        const REQUEST_ADRESS = `${BASE_URL}/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=${releaseDateDesc}&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate`
         return baseFetch(REQUEST_ADRESS);
     } else if (releaseDateAsk === 'primary_release_date.asc') {
-        const REQUEST_ADRESS = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=${releaseDateAsk}&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate`
+        const REQUEST_ADRESS = `${BASE_URL}/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=${releaseDateAsk}&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate`
         return baseFetch(REQUEST_ADRESS);
     } else if (releaseTitleDesc === 'original_title.desc') {
-        const REQUEST_ADRESS = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=${releaseTitleDesc}&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate`
+        const REQUEST_ADRESS = `${BASE_URL}/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=${releaseTitleDesc}&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate`
         return baseFetch(REQUEST_ADRESS);
     }
 
     else if (releaseTitleAsk === 'original_title.asc') {
-        const REQUEST_ADRESS = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=${releaseTitleAsk}&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate`
+        const REQUEST_ADRESS = `${BASE_URL}/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=${releaseTitleAsk}&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate`
         return baseFetch(REQUEST_ADRESS);
     }
 
