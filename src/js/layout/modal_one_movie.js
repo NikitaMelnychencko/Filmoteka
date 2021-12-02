b65cd2a9a054f263941ee6198e1563871e9773bb
Yevheniia Mankivska <evgeniyamankovskaya@gmail.com>
2021-12-02 09:00:26 +0200

    Update modal_one_movie.js

--- src/js/layout/modal_one_movie.js
+++ src/js/layout/modal_one_movie.js
@@ -1,7 +1,12 @@
 import modal_one_movie_markup from '../../views/partials/modal_one_movie.hbs';
 import { renderModal, closeModal } from '../components/modal';
 import { renderParamsCard } from '../components/fetch';
-import { postUserData, userId,deleteData,getIdUser} from '../components/appFirebase.js';
+import {
+  postUserData,
+  userId,
+  deleteData,
+  getIdUser,
+} from '../components/appFirebase.js';
 import img from '../../images/img/png/gallery/no-image.png';
 import { refs } from '../refs/refs.js';
 import { mouseUp } from '../components/modal_login.js';
@@ -12,20 +17,19 @@ const IMG_URL = 'https://image.tmdb.org/t/p/w500';
 
 function refButton() {
   const buttonList = document.querySelector('.modal-one-movie__button-box');
-  return buttonList
+  return buttonList;
 }
 
 function renderMovieSeorchParam(id) {
-  renderParamsCard(id)
-    .then(data => {
-      renderModal(modal_one_movie_markup(imgFix(data)));
-      objService = data;
-      arrObj = JSON.stringify({ objService });
-      localStorage.setItem('idFilm', id);
-      localStorage.setItem('marcupFilm', arrObj);
-      addToDataBase(imgFix(data));
-      updateButton(id);
-    })
+  renderParamsCard(id).then(data => {
+    renderModal(modal_one_movie_markup(imgFix(data)));
+    objService = data;
+    arrObj = JSON.stringify({ objService });
+    localStorage.setItem('idFilm', id);
+    localStorage.setItem('marcupFilm', arrObj);
+    addToDataBase(imgFix(data));
+    updateButton(id);
+  });
 }
 
 function imgFix(m) {
@@ -35,18 +39,17 @@ function imgFix(m) {
   };
 }
 export function updateButton(id) {
-  const watched = getIdUser(userId, 'watched', id)
-  const queue = getIdUser(userId, 'queue', id)
+  const watched = getIdUser(userId, 'watched', id);
+  const queue = getIdUser(userId, 'queue', id);
   Promise.all([watched, queue]).then(values => {
-    values.forEach((item,index) => {
+    values.forEach((item, index) => {
       if (item === null) {
-        return
+        return;
       } else {
-        refButton().children[index].disabled = true
-        refButton().children[index].style.background = 'grey'
-        refButton().children[index].style.color='white'
+        refButton().children[index].style.background = 'grey';
+        refButton().children[index].style.color = 'white';
       }
-    })
+    });
   });
 }
 
@@ -70,7 +73,7 @@ function addToDataBase(data) {
     if (e.target.nodeName !== 'BUTTON') return;
     if (userId == null) {
       refs.singinModal.classList.remove('modal-auth--hidden');
-      mouseUp()
+      mouseUp();
     } else {
       postUserData(userId, e.target.ariaLabel, idFilm, markupFilm);
       deleteData(userId, e.target.dataset.set, idFilm);
